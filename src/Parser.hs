5d60c9619e4004bfe00385046aab08a11b440dd1
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-11 22:49:08 -0500

    Reimplements the BF parser using ReaderT
    1. Changes the type of Dictionary to remove the use of Map, since the
       token list is not that large. Also adds a flag for when all of the
       tokens are one character in length.
    2. Refactors the parsers so that they do not simply match prefixes
       but rather up to the next separator.

--- src/Parser.hs
+++ src/Parser.hs
@@ -4,10 +4,12 @@ module Parser
     ( parse
     ) where
 
-import qualified Types      as T
 import qualified Data.Text  as Tx
-import qualified Data.Map   as Map
 import qualified Data.Char  as C
+import Control.Monad.Reader         ( ReaderT (..)
+                                    , runReaderT
+                                    , lift          )
+import Types
 import Data.List                    ( maximumBy
                                     , minimumBy
                                     , find          )
@@ -17,113 +19,117 @@ import Control.Applicative          ( Alternative
                                     , many, empty   )
 
 ---------------------------------------------------------------------
+-- Entry point
 
-parse :: T.Dictionary -> Text -> Maybe T.Program
-parse d = maybe Nothing ( \ (_,_,x) -> Just x ) . T.runParser program d
+parse :: Dictionary -> Text -> Maybe Program
+parse d t = maybe Nothing ( \ (_,x) -> Just x )
+            . runParser ( runReaderT program d ) $ t
 
-program :: T.Parser T.Program
+---------------------------------------------------------------------
+-- Parsers
+
+program :: BFParser Program
 program = do
     p <- many statement
     endOfInput
     pure p
 
-statement :: T.Parser T.Statement
-statement = do
-    separator
-    kw <- opening
-    case kw of
-         T.BFStart -> routine >>= pure . T.WhileLoop
-         T.BFHash  -> skipLine >> pure T.DoNothing
-         otherwise -> pure . toStatement $ kw
-
-endOfInput :: T.Parser ()
-endOfInput = T.Parser $ \ d s ->
-    if Tx.null s
-       then Just (d, Tx.empty, ())
-       else Nothing
-
-opening :: T.Parser T.Token
-opening = choice . map token $ axns
-    where axns = [ T.BFGT,  T.BFLT,    T.BFPlus,  T.BFMinus
-                 , T.BFDot, T.BFComma, T.BFStart, T.BFHash
-                 ]
-
-routine :: T.Parser T.Program
-routine = do
+subProgram :: BFParser Program
+subProgram = do
     p <- many statement
     stop >> pure p
 
-choice :: [T.Parser a] -> T.Parser a
+statement :: BFParser Statement
+statement = do
+    spaces
+    kw <- opening
+    case kw of
+         BFStart -> subProgram >>= pure . WhileLoop
+         BFHash  -> skipLine >> pure DoNothing
+         otherwise -> pure . toStatement $ kw
+
+opening :: BFParser Token
+opening = choice . map token $ [ BFGT,  BFLT,    BFPlus,  BFMinus
+                               , BFDot, BFComma, BFStart, BFHash
+                               ]
+
+stop :: BFParser Token
+stop = token BFStop
+
+start :: BFParser Token
+start = token BFStart
+
+spaces :: BFParser ()
+spaces = many ( satisfy C.isSpace ) >> pure ()
+
+---------------------------------------------------------------------
+-- Base parsers
+
+choice :: [BFParser a] -> BFParser a
 choice []     = empty
 choice (x:[]) = x
 choice (x:xs) = x <|> choice xs
 
-stop :: T.Parser T.Token
-stop = token T.BFStop
+endOfInput :: BFParser ()
+endOfInput = lift . Parser $ \ s ->
+    if Tx.null s
+       then Just (Tx.empty, ())
+       else Nothing
 
-start :: T.Parser T.Token
-start = token T.BFStart
+token :: Token -> BFParser Token
+token tok = ReaderT $ \ d -> Parser $ \ s ->
+    do -- This is a Maybe monad
+    let (next, rest) = splitNext d s
+    strings <- lookup tok . tokens $ d
+    if elem next strings
+       then Just (rest, tok)
+       else Nothing
 
-token :: T.Token -> T.Parser T.Token
-token tok = T.Parser $ \ d s ->
-    case findPrefix d tok s of
-         Nothing -> Nothing
-         Just x  -> let s' = Tx.drop (Tx.length x) s
-                    in  Just ( d, s', tok )
+satisfy :: (Char -> Bool) -> BFParser Char
+satisfy f = lift . Parser $ \ s ->
+    do -- This is a Maybe monad
+    (t, ts) <- Tx.uncons s
+    if f t
+       then Just (ts, t)
+       else Nothing
 
-separator :: T.Parser ()
-separator = many ( satisfy C.isSpace ) >> pure ()
-
-satisfy :: (Char -> Bool) -> T.Parser Char
-satisfy f = T.Parser $ \ d s ->
-    case Tx.uncons s of
-         Nothing     -> Nothing
-         Just (t,ts) -> if f t then Just (d, ts, t) else Nothing
-
-skipLine :: T.Parser ()
-skipLine = T.Parser $ \ d s ->
+skipLine :: BFParser ()
+skipLine = lift . Parser $ \ s ->
     case Tx.uncons . Tx.dropWhile (/= '\n') $ s of
-         Nothing      -> Just (d, Tx.empty, ())
-         Just (_, s') -> Just (d, s',       ())
+         Nothing      -> Just (Tx.empty, ())
+         Just (_, s') -> Just (s',       ())
 
-toStatement :: T.Token -> T.Statement
-toStatement T.BFGT    = T.Advance
-toStatement T.BFLT    = T.Backup
-toStatement T.BFPlus  = T.Increment
-toStatement T.BFMinus = T.Decrement
-toStatement T.BFComma = T.ReadIn
-toStatement T.BFDot   = T.WriteOut
-toStatement _         = T.DoNothing
+---------------------------------------------------------------------
+-- Helpers
 
-bfDict :: T.Dictionary
-bfDict =  Map.fromList [ ( T.BFGT,    [">"] )
-                       , ( T.BFLT,    ["<"] )
-                       , ( T.BFPlus,  ["+"] )
-                       , ( T.BFMinus, ["-"] )
-                       , ( T.BFDot,   ["."] )
-                       , ( T.BFComma, [","] )
-                       , ( T.BFStart, ["["] )
-                       , ( T.BFStop,  ["]"] )
-                       , ( T.BFHash,  ["#"] )
+toStatement :: Token -> Statement
+toStatement BFGT    = Advance
+toStatement BFLT    = Backup
+toStatement BFPlus  = Increment
+toStatement BFMinus = Decrement
+toStatement BFComma = ReadIn
+toStatement BFDot   = WriteOut
+toStatement _       = DoNothing
+
+splitNext :: Dictionary -> Text -> (Text, Text)
+splitNext d
+    | isShort d = Tx.splitAt 1
+    | otherwise = Tx.break C.isSpace
+
+---------------------------------------------------------------------
+-- For testing
+
+bfDict :: Dictionary
+bfDict =  toDictionary [ ( BFGT,    [">"] )
+                       , ( BFLT,    ["<"] )
+                       , ( BFPlus,  ["+"] )
+                       , ( BFMinus, ["-"] )
+                       , ( BFDot,   ["."] )
+                       , ( BFComma, [","] )
+                       , ( BFStart, ["["] )
+                       , ( BFStop,  ["]"] )
+                       , ( BFHash,  ["#"] )
                        ]
 
 testProg :: Text
 testProg = "++> ++[<+>-]-->> # a comment \n <<[<>--++[+<->+]]->+"
----------------------------------------------------------------------
-
-findPrefix :: T.Dictionary -> T.Token -> Text -> Maybe Text
-findPrefix d tok txt = case Map.lookup tok d of
-                            Nothing -> Nothing
-                            Just xs -> let mn = minLength xs
-                                           mx = maxLength xs
-                                       in find (flip elem xs)
-                                          $ [ Tx.take n txt | n <- [mn..mx] ]
-
-compareLength :: Text -> Text -> Ordering
-compareLength x y = compare ( Tx.length x ) ( Tx.length y )
-
-maxLength :: [Text] -> Int
-maxLength = Tx.length . maximumBy compareLength
-
-minLength :: [Text] -> Int
-minLength = Tx.length . minimumBy compareLength
--- src/Types.hs
+++ src/Types.hs
@@ -8,11 +8,16 @@ module Types
     , Program     (..)
     , BFScript    (..)
     , Computation (..)
-    , Dictionary  (..)
+    , Dictionary
+    , BFParser    (..)
+    , toDictionary
+    , tokens
+    , isShort
     ) where
 
 import qualified Data.ByteString as B
-import qualified Data.Map        as Map
+import qualified Data.Text       as Tx
+import Control.Monad.Reader             ( ReaderT       )
 import Data.Text                        ( Text          )
 import Data.Word                        ( Word8         )
 import Control.Applicative              ( Alternative
@@ -34,26 +39,28 @@ instance Foldable Tape where
 
 ---------------------------------------------------------------------
 
-newtype Parser a = Parser {
-    runParser :: Dictionary -> Text -> Maybe (Dictionary, Text, a) }
+-- This is the same as StateT Text Maybe a
+-- Needs to be refactored
+
+newtype Parser a = Parser { runParser :: Text -> Maybe (Text, a) }
 
 instance Functor Parser where
-    fmap f (Parser p) = Parser $ \ d s -> do (_, s', x) <- p d s
-                                             Just (d, s', f x)
+    fmap f (Parser p) = Parser $ \ s -> do (s', x) <- p s
+                                           Just (s', f x)
 
 instance Applicative Parser where
-    pure x                  = Parser $ \ d s -> Just (d, s, x)
-    Parser pl <*> Parser pr = Parser $ \ d s -> do (_, s1, f) <- pl d s
-                                                   (_, s2, x) <- pr d s1
-                                                   Just (d, s2, f x)
+    pure x                  = Parser $ \ s -> Just (s, x)
+    Parser pl <*> Parser pr = Parser $ \ s -> do (s1, f) <- pl s
+                                                 (s2, x) <- pr s1
+                                                 Just (s2, f x)
 
 instance Alternative Parser where
-    empty                   = Parser $ \ _ _ -> Nothing
-    Parser pl <|> Parser pr = Parser $ \ d s -> pl d s <|> pr d s
+    empty                   = Parser $ \ _ -> Nothing
+    Parser pl <|> Parser pr = Parser $ \ s -> pl s <|> pr s
 
 instance Monad Parser where
-    Parser p >>= g = Parser $ \ d s -> do (d, s1, x) <- p d s
-                                          runParser (g x) d s1
+    Parser p >>= g = Parser $ \ s -> do (s1, x) <- p s
+                                        runParser (g x) s1
 
 ---------------------------------------------------------------------
 
@@ -83,8 +90,18 @@ data Token = BFGT
            | BFHash
              deriving ( Ord, Eq, Show )
 
+data Dictionary = Dictionary { tokens  :: [ (Token, [Text]) ]
+                             , isShort :: Bool
+                             }
+
+toDictionary :: [(Token, [Text])] -> Dictionary
+toDictionary ts = Dictionary ts $ all ( (== 1) . Tx.length ) $ xs
+    where xs = concat . snd . unzip $ ts
+
 type Program     = [Statement]
 type ErrString   = String
 type Computation = Computer -> Either ErrString Computer
 type BFScript    = [Token]
-type Dictionary  = Map.Map Token [Text]
+
+-- | BFParser a = ReaderT ( Dictionary -> Parser a )
+type BFParser    = ReaderT Dictionary Parser
