78ea281ce812ba9e322d4037797e3f374d84c983
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-16 19:28:24 -0500

    Adds a working Controller and Main
    This is now a working, basic BF-interpreter.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,17 @@
 module Main where
 
-import Types
+import qualified Data.ByteString as B
+import qualified Types           as T
+import Data.Text                        ( Text          )
+import System.Environment               ( getArgs       )
+import Controller                       ( execute
+                                        , formatOutput
+                                        , getScript     )
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    script <- getArgs >>= getScript
+    case script >>= execute B.empty of
+         Left e  -> putStrLn $ "Error: " ++ e
+         Right c -> let result = formatOutput . T.output $ c
+                    in  putStrLn result
---
+++ src/Controller.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Controller
+    ( execute
+    , getScript
+    , formatOutput
+    ) where
+
+import qualified Data.Word       as W
+import qualified Data.ByteString as B
+import qualified Data.Text.IO    as Tx
+import qualified Data.Text       as Tx
+import qualified Types           as T
+import System.Directory                 ( doesFileExist )
+import Data.Text                        ( Text       )
+import Compiler                         ( runProgram )
+import Parser                           ( parse      )
+
+execute :: B.ByteString -> Text -> Either T.ErrString T.Computer
+execute b t = parse bfDict t >>= runProgram ( initComputer b )
+
+getScript :: [String] -> IO (Either T.ErrString Text)
+getScript []    = pure $ Left "A script file is required"
+getScript (x:_) = do
+    exists <- doesFileExist x
+    if exists
+       then Right <$> Tx.readFile x
+       else pure . Left $ "Cannot find script file " ++ x
+
+formatOutput :: B.ByteString -> String
+formatOutput = map ( toEnum . fromIntegral ) . B.unpack
+
+initComputer b = T.Computer { T.input  = b
+                            , T.output = B.empty
+                            , T.memory = T.Tape [] 0 []
+                            }
+
+bfDict :: T.Dictionary
+bfDict = T.dictionary [ ( T.BFGT,    [">"] )
+                      , ( T.BFLT,    ["<"] )
+                      , ( T.BFPlus,  ["+"] )
+                      , ( T.BFMinus, ["-"] )
+                      , ( T.BFDot,   ["."] )
+                      , ( T.BFComma, [","] )
+                      , ( T.BFStart, ["["] )
+                      , ( T.BFStop,  ["]"] )
+                      , ( T.BFHash,  ["#"] )
+                      ]
