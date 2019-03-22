979e6bdb97b55b8f386df0a2b8a46c74b2c6505c
MWRuszczycky <mwruszczycky@gmail.com>
2019-03-22 18:21:00 -0500

    Introduces Brick

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
-import qualified Data.ByteString as B
+import qualified Data.ByteString as BS
 import qualified Model.Types     as T
+import qualified Graphics.Vty    as V
+import qualified Brick           as B
 import System.Environment               ( getArgs       )
 import Controller                       ( execute       )
 import Loader                           ( formatOutput
@@ -10,6 +12,14 @@ import Loader                           ( formatOutput
 main :: IO ()
 main = do
     script <- getArgs >>= getScript
-    case script >>= execute B.empty of
+    case script >>= execute BS.empty of
          Left e  -> putStrLn $ "Error: " ++ e
          Right c -> putStrLn . formatOutput . T.output $ c
+
+initialize :: B.App T.Computer e ()
+initialize = B.App { B.appDraw         = const []
+                   , B.appHandleEvent  = B.resizeOrQuit
+                   , B.appChooseCursor = B.neverShowCursor
+                   , B.appStartEvent   = pure
+                   , B.appAttrMap      = const (B.attrMap V.defAttr [])
+                   }
