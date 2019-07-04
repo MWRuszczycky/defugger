e9657fbd5c7106f0e0751943b3a91f15cc8976b8
MWRuszczycky <mwruszczycky@gmail.com>
2019-07-04 13:40:06 -0500

    Adds the tryWriteFile function
    Also refactors CoreIO.

--- src/Model/CoreIO.hs
+++ src/Model/CoreIO.hs
@@ -1,6 +1,7 @@
 module Model.CoreIO
     ( tryReadFile
     , tryReadBytes
+    , tryWriteFile
     ) where
 
 import qualified Data.ByteString as BS
@@ -12,13 +13,16 @@ import Control.Exception                ( IOException
                                         , catch        )
 
 tryReadFile :: FilePath -> T.ErrorIO Text
-tryReadFile fp = ExceptT $ do
-    catch ( Right <$> Tx.readFile fp ) hndlErr
+tryReadFile fp = ExceptT $ catch ( Right <$> Tx.readFile fp ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString Text )
           hndlErr = pure . Left . show
 
+tryWriteFile :: FilePath -> Text -> T.ErrorIO ()
+tryWriteFile fp t = ExceptT $ catch ( Right <$> Tx.writeFile fp t ) hndlErr
+    where hndlErr :: IOException -> IO ( Either T.ErrString () )
+          hndlErr = pure . Left . show
+
 tryReadBytes :: FilePath -> T.ErrorIO BS.ByteString
-tryReadBytes fp = ExceptT $ do
-    catch ( Right <$> BS.readFile fp ) hndlErr
+tryReadBytes fp = ExceptT $ catch ( Right <$> BS.readFile fp ) hndlErr
     where hndlErr :: IOException -> IO ( Either T.ErrString BS.ByteString )
           hndlErr = pure . Left . show
