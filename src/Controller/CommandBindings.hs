d92203c717ae55f791690cecb5f0b6215ceb41c9
MWRuszczycky <mwruszczycky@gmail.com>
2019-07-14 13:56:12 -0500

    Adds framework for the :open and :save commands
    1. The commands are not yet implemented and just display an error to
       this effect if you try to use them.
    2. The :s and :u abbreviations for :set and :unset were deprecated.
       The :s abbreviation will now be used for :save.

--- src/Controller/CommandBindings.hs
+++ src/Controller/CommandBindings.hs
@@ -37,9 +37,11 @@ commands :: [T.CommandBinding]
 -- ^Organizes all the commands that can be run from the debugger.
 commands = [ T.CommandBinding ["help",  "h"] help_action  help_help
            , T.CommandBinding ["load",  "l"] load_action  load_help
+           , T.CommandBinding ["open",  "o"] open_action  open_help
            , T.CommandBinding ["reset", "r"] reset_action reset_help
-           , T.CommandBinding ["set",   "s"] set_action   set_help
-           , T.CommandBinding ["unset", "u"] unset_action unset_help
+           , T.CommandBinding ["save",  "s"] save_action  save_help
+           , T.CommandBinding ["set"       ] set_action   set_help
+           , T.CommandBinding ["unset"     ] unset_action unset_help
            , T.CommandBinding ["write", "w"] write_action write_help
            , T.CommandBinding ["quit",  "q"] quit_action  quit_help
            ]
@@ -76,6 +78,23 @@ load_action (x:y:_) = T.SimpleIOCmd $ reloadDebugger ( Just . Tx.unpack $ x )
 load_action (x:_)   = T.SimpleIOCmd $ reloadDebugger ( Just . Tx.unpack $ x )
                                                      Nothing
 
+-- open -------------------------------------------------------------
+
+open_help :: T.HelpInfo
+open_help = T.HelpInfo ns us sh (Tx.unlines lh)
+    where ns = [ "open", "o" ]
+          us = ":open FILEPATH"
+          sh = "Opens a debugger state file."
+          lh = [ "Use the :open command to load script, memory, output and"
+               , "input state information into the debugger. This information"
+               , "can be generated using the :save command creating .defug"
+               , "files. See also the :save, :write and :load commands."
+               ]
+
+open_action :: T.CommandAction
+open_action _ = T.ErrorCmd "The :open command is not yet available."
+
+
 -- reset ------------------------------------------------------------
 
 reset_help :: T.HelpInfo
@@ -88,11 +107,30 @@ reset_help = T.HelpInfo ns us sh lh
 reset_action :: T.CommandAction
 reset_action _ = T.PureCmd $ resetDebugger
 
+-- save -------------------------------------------------------------
+
+save_help :: T.HelpInfo
+save_help = T.HelpInfo ns us sh (Tx.unlines lh)
+    where ns = [ "save", "s" ]
+          us = ":save [FILEPATH]"
+          sh = "Saves the computer state at the current point of evaluation."
+          lh = [ "Use the :save command to save the script, memory, output and"
+               , "input states at current point of BF script evaluation to a"
+               , "human-readable file. The file path to save to can be"
+               , "specified as the FILEPATH argument. If no argument is"
+               , "provided, then the current script path is used with the"
+               , "extension .defug. See also help for the :open and :write"
+               , "commands."
+               ]
+
+save_action :: T.CommandAction
+save_action _ = T.ErrorCmd "The :save command is not yet available."
+
 -- set --------------------------------------------------------------
 
 set_help :: T.HelpInfo
 set_help = T.HelpInfo ns us sh lh
-    where ns = [ "set", "s" ]
+    where ns = [ "set" ]
           us = ":set [SETTING [VALUE..]]"
           sh = "Sets a property in the Defugger"
           lh = Tx.empty
@@ -104,7 +142,7 @@ set_action = either T.ErrorCmd T.PureCmd . parseSet
 
 unset_help :: T.HelpInfo
 unset_help = T.HelpInfo ns us sh lh
-    where ns = [ "unset", "u" ]
+    where ns = [ "unset" ]
           us = ":unset [SETTING [VALUE..]]"
           sh = "Unsets a property in the Defugger"
           lh = Tx.empty
