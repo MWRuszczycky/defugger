8f8848b312a35b1d677d8f9cd2c4a04497fd7a8b
MWRuszczycky <mwruszczycky@gmail.com>
2019-07-21 20:20:28 -0500

    Adds tests for the faster interpreter

--- tests/ModelTest/InterpreterTests.hs
+++ tests/ModelTest/InterpreterTests.hs
@@ -23,7 +23,7 @@ type Result = (T.Computer, T.DBProgram, Set.Set Int, BS.ByteString)
 
 spec :: Spec
 spec = do
-    describe "Running interpreter with the default dictionary" $ do
+    describe "Running default interpreter with the default dictionary" $ do
         it "Correctly runs HelloWorld.bf" $ do
             scriptNoInput ( U.getTestPath "HelloWorld.bf"     )
                           ( U.getTestPath "TestSave104.defug" )
@@ -37,6 +37,26 @@ spec = do
             scriptWithInput ( U.getTestPath "WriteHelloWorld.bf"  )
                             ( U.getTestPath "WriteHelloWorld.txt" )
                             ( U.getTestPath "TestSave105.defug"   )
+    describe "Running fast interpreter with the default dictionary" $ do
+        it "Correctly runs HelloWorld.bf" $ do
+            scriptNoInputFast ( U.getTestPath "HelloWorld.bf"     )
+                          ( U.getTestPath "TestSave104.defug" )
+        it "Correctly runs WriteHelloWorld.bf (input test)" $ do
+            scriptWithInputFast ( U.getTestPath "WriteHelloWorld.bf"  )
+                            ( U.getTestPath "WriteHelloWorld.txt" )
+                            ( U.getTestPath "TestSave105.defug"   )
+
+---------------------------------------------------------------------
+-- Helpers
+
+checkResult :: FilePath -> Either T.ErrString Result -> IO ()
+checkResult _  (Left  e ) = error $ "Test failed: " ++ e
+checkResult fp (Right r ) = do
+    expected <- BS.readFile fp
+    D.encodeResult r `shouldBe` expected
+
+---------------------------------------------------------------------
+-- Testing the slower interpreter
 
 scriptNoInput :: FilePath -> FilePath -> IO ()
 scriptNoInput s t = runExceptT (args >>= SU.parseOptions) >>= either err go
@@ -58,8 +78,27 @@ scriptWithInput s i t = runExceptT (args >>= SU.parseOptions) >>= either err go
                                              >>= checkResult t
                          _                -> goError
 
-checkResult :: FilePath -> Either T.ErrString Result -> IO ()
-checkResult _  (Left  e ) = error $ "Test failed: " ++ e
-checkResult fp (Right r ) = do
-    expected <- BS.readFile fp
-    D.encodeResult r `shouldBe` expected
+---------------------------------------------------------------------
+-- Testing the faster interpreter
+
+scriptNoInputFast :: FilePath -> FilePath -> IO ()
+scriptNoInputFast s t = runExceptT (args >>= SU.parseOptions)
+                        >>= either err go
+    where args    = pure ["--run", "--fast", s]
+          err e   = error $ "Test failed: Unable to read options: " ++ e
+          goError = error "Test failed: RunInterpreter mode expected"
+          go opts = case T.runMode opts of
+                         T.RunInterpreter -> runExceptT (SU.interpreter opts)
+                                             >>= checkResult t
+                         _                -> goError
+
+scriptWithInputFast :: FilePath -> FilePath -> FilePath -> IO ()
+scriptWithInputFast s i t = runExceptT (args >>= SU.parseOptions)
+                            >>= either err go
+    where args    = pure ["--run", "--fast", s, i]
+          err e   = error $ "Test failed: Unable to read options: " ++ e
+          goError = error "Test failed: RunInterpreter mode expected"
+          go opts = case T.runMode opts of
+                         T.RunInterpreter -> runExceptT (SU.interpreter opts)
+                                             >>= checkResult t
+                         _                -> goError
